d46ec2edb12ee4f6ee7b915ef3fc5ab6ec8e59b6
arick_yumol <arick.yumol@gmail.com>
2021-09-07 21:22:10 +0800

    s24 activity

--- a1/activity.js
+++ a1/activity.js
@@ -1,10 +1,14 @@
 /*
+
+#2	Use $or, and hide _id field
 db.users.find( { $or: [ { firstName: { $regex: "S" } }, { lastName: { $regex: "D" } } ] }, {_id: 0, firstName: 1, lastName: 1 }, { firstName: 1, lastName: 1, _id: 0} ).pretty();
 
 
+#3	Use $and
 db.users.find({ $and: [{ department: "HR" }, { age: { $gte: 70 } } ] }).pretty();
 
 
+#4	Use #and, $regex, and $lte
 db.users.find({ $and: [{ firstName: {$regex: 'e'} }, { age: { $lte: 30 } } ] }).pretty();
 
 
